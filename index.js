888481806a79277db722d7328cfdf8ca13f0d99c
Noel S. David - CS <noelsdavid@gmail.com>
2016-11-18 14:46:53 +0800

    separate concerns

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
 
-app.get('/',function(req,res){
- res.send('Hello Noel David. ');
-});
+//app.get('/',function(req,res){
+// res.send('Hello Noel David. ');
+//});
+//
+//app.get('/kumusta',function(req,res){
+// res.send('Hello Noel David. ');
+//});
 
-app.listen(3000);
+var things = require('./things.js'); 
+//both index.js and things.js should be in same directory
+app.use('/things', things); 
+
+app.listen(3000,function(){
+    console.log('Server running at http://localhost:3000');
+});
