976a943bee758ca643e9b5599e64c5c09fa6377c
Noel David <noelsdavid@gmail.com>
2016-11-28 10:19:07 +0800

    database

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var multer = require('multer');
+var app = express();
+app.set('view engine','pug');
+// app.set('view engine', 'form');
+app.set('views', './views');
+
+// var mysql = require('mysql')
+// var connection = mysql.createConnection({
+//   host: '127.0.0.1',
+//   user: 'root',
+//   password: 'root'
+// })
+
+// connection.connect()
+
+// connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+//   if (err) throw err
+
+//   console.log('The solution is: ', rows[0].solution)
+// })
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/my_db');
+mongoose.connect('mongodb://127.0.0.1/my_db');
 
 var personSchema = mongoose.Schema({
     name: String,
@@ -12,9 +29,28 @@ var personSchema = mongoose.Schema({
 });
 var Person = mongoose.model("Person", personSchema);
 
-app.post('/', function(req, res){
-    console.log(req.body);
-    res.send("recieved your request!");
+app.get('/person', function(req, res){
+    res.render('person');
+});
+
+app.post('/person', function(req, res){
+    var personInfo = req.body; //Get the parsed information
+    if(!personInfo.name || !personInfo.age || !personInfo.nationality){
+        res.render('show_message', {message: "Sorry, you provided worng info", type: "error"});
+    }
+    else{
+        var newPerson = new Person({
+            name: personInfo.name,
+            age: personInfo.age,
+            nationality: personInfo.nationality
+        });
+        newPerson.save(function(err, res){
+            if(err)
+                res.render('show_message', {message: "Database error", type: "error"});
+            else
+                res.render('show_message', {message: "New person added", type: "success", person: personInfo});
+        });
+    }
 });
 
 app.listen(3000);
